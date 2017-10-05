3ea510d221736d3f61fb437f57f78e33b5d0c1df
Peter Moraes <peter.veiga.moraes@gmail.com>
2017-10-05 13:21:12 -0300

    refs #33 fixing eol in bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,1 +1,25 @@
-<?php/** * Copyright 2016, Cake Development Corporation (http://cakedc.com) * * Licensed under The MIT License * Redistributions of files must retain the above copyright notice. * * @copyright Copyright 2016, Cake Development Corporation (http://cakedc.com) * @license MIT License (http://www.opensource.org/licenses/mit-license.php) */use Cake\Core\Configure;Configure::load('CakeDC/Api.api');collection((array)Configure::read('Api.config'))->each(function ($file) {    Configure::load($file);});Log::setConfig('api', [    'className' => 'File',    'path' => LOGS,    'scopes' => ['api'],    'levels' => ['error', 'info'],    'file' => 'api.log',]);
+<?php
+/**
+ * Copyright 2016-2017, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2016, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+use Cake\Core\Configure;
+
+Configure::load('CakeDC/Api.api');
+collection((array)Configure::read('Api.config'))->each(function ($file) {
+    Configure::load($file);
+});
+
+Log::setConfig('api', [
+    'className' => 'File',
+    'path' => LOGS,
+    'scopes' => ['api'],
+    'levels' => ['error', 'info'],
+    'file' => 'api.log',
+])
