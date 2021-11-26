082ccfb1597ed85ad315656a55ee9ecc65e082f4
Antonio Reveron <a.reveron@gf-tic.com>
2021-11-26 17:43:03 +0000

    REF #92 improved Action::getData by passing it a key and get the value

--- src/Service/Action/Action.php
+++ src/Service/Action/Action.php
@@ -311,11 +311,22 @@ abstract class Action implements EventListenerInterface, EventDispatcherInterfac
     /**
      * Returns action input params
      *
-     * @return array
+     * @param string|null $name Dot separated name of the value to read. Or null to read all data.
+     * @param mixed $default The default data.
+     * @return mixed The value being read.
      */
-    public function getData(): array
+    public function getData($name = null, $default = null)
     {
-        return $this->getService()->getParser()->getParams();
+        $data = $this->getService()->getParser()->getParams();
+        if ($name === null) {
+            return $data;
+        }
+        if (!is_array($data) && $name) {
+            return $default;
+        }
+
+        /** @psalm-suppress PossiblyNullArgument */
+        return Hash::get($data, $name, $default);
     }
 
     /**
