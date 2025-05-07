25b0ec5a937e3e9c7a20de25188aef2bfc0158f3
Rodrigo Pérez <rodrigo.perez@cakedc.com>
2025-05-07 10:39:02 +0100

    Migrate webatuh lib to version 4.4

--- src/Webauthn/AuthenticateAdapter.php
+++ src/Webauthn/AuthenticateAdapter.php
@@ -13,7 +13,10 @@ declare(strict_types=1);
 
 namespace CakeDC\Api\Webauthn;
 
-use Cake\Log\Log;
+use Cake\Http\Exception\BadRequestException;
+use Webauthn\AuthenticationExtensions\AuthenticationExtensionsClientInputs;
+use Webauthn\AuthenticatorAssertionResponse;
+use Webauthn\AuthenticatorAssertionResponseValidator;
 use Webauthn\PublicKeyCredentialRequestOptions;
 use Webauthn\PublicKeyCredentialSource;
 
@@ -25,22 +28,20 @@ class AuthenticateAdapter extends BaseAdapter
     public function getOptions(): PublicKeyCredentialRequestOptions
     {
         $userEntity = $this->getUserEntity();
-        $allowed = array_map(function (PublicKeyCredentialSource $credential) {
+        $allowedCredentials = array_map(function (PublicKeyCredentialSource $credential) {
             return $credential->getPublicKeyCredentialDescriptor();
         }, $this->repository->findAllForUserEntity($userEntity));
-        Log::error(print_r($allowed, true));
 
-        $options = $this->server->generatePublicKeyCredentialRequestOptions(
-            PublicKeyCredentialRequestOptions::USER_VERIFICATION_REQUIREMENT_PREFERRED,
-            $allowed
-        );
+        $options = (new PublicKeyCredentialRequestOptions(random_bytes(32)))
+            ->setRpId($this->rpEntity->getId())
+            ->setUserVerification(PublicKeyCredentialRequestOptions::USER_VERIFICATION_REQUIREMENT_PREFERRED)
+            ->allowCredentials(...$allowedCredentials)
+            ->setExtensions(new AuthenticationExtensionsClientInputs());
+
         $storeEntity = $this->readStore();
-        Log::error(print_r($storeEntity, true));
         $storeEntity['store'] = [];
         $storeEntity = $this->patchStore($storeEntity, 'authenticateOptions', base64_encode(serialize($options)));
         $res = $this->store->save($storeEntity);
-        Log::error(print_r($storeEntity, true));
-        Log::error(print_r($res, true));
 
         return $options;
     }
@@ -49,31 +50,44 @@ class AuthenticateAdapter extends BaseAdapter
      * Verify the registration response
      *
      * @return \Webauthn\PublicKeyCredentialSource
+     * @throws \Throwable
      */
     public function verifyResponse(): \Webauthn\PublicKeyCredentialSource
     {
         $storeEntity = $this->readStore();
-        Log::error(print_r($storeEntity, true));
         $options = $this->getStore($storeEntity, 'authenticateOptions');
         if ($options) {
             $options = unserialize(base64_decode($options));
         }
-        Log::error(print_r($options, true));
 
-        return $this->loadAndCheckAssertionResponse($options);
+        $publicKeyCredentialLoader = $this->createPublicKeyCredentialLoader();
+        $publicKeyCredential = $publicKeyCredentialLoader->loadArray($this->request->getData());
+        $authenticatorAssertionResponse = $publicKeyCredential->getResponse();
+        if ($authenticatorAssertionResponse instanceof AuthenticatorAssertionResponse) {
+            $authenticatorAssertionResponseValidator = $this->createAssertionResponseValidator();
+
+            return $authenticatorAssertionResponseValidator->check(
+                $publicKeyCredential->getRawId(),
+                $authenticatorAssertionResponse,
+                $options,
+                $this->request,
+                $this->getUserEntity()->getId(),
+            );
+        }
+
+        throw new BadRequestException(__d('cake_d_c/api', 'Could not validate credential response for authentication'));
     }
 
     /**
-     * @param \Webauthn\PublicKeyCredentialRequestOptions $options request options
-     * @return \Webauthn\PublicKeyCredentialSource
+     * @return \Webauthn\AuthenticatorAssertionResponseValidator
      */
-    protected function loadAndCheckAssertionResponse($options): PublicKeyCredentialSource
+    protected function createAssertionResponseValidator(): AuthenticatorAssertionResponseValidator
     {
-        return $this->server->loadAndCheckAssertionResponse(
-            json_encode($this->request->getData()),
-            $options,
-            $this->getUserEntity(),
-            $this->request
+        return new AuthenticatorAssertionResponseValidator(
+            $this->repository,
+            null,
+            $this->createExtensionOutputCheckerHandler(),
+            $this->getAlgorithmManager()
         );
     }
 }
--- src/Webauthn/Repository/UserCredentialSourceRepository.php
+++ src/Webauthn/Repository/UserCredentialSourceRepository.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace CakeDC\Api\Webauthn\Repository;
 
-use Base64Url\Base64Url;
 use Cake\Datasource\EntityInterface;
 use Cake\Http\ServerRequest;
 use Cake\Utility\Hash;
 use CakeDC\Api\Utility\RequestParser;
 use CakeDC\Users\Model\Table\UsersTable;
+use CakeDC\Users\Webauthn\Base64Utility;
 use Webauthn\PublicKeyCredentialSource;
 use Webauthn\PublicKeyCredentialSourceRepository;
 use Webauthn\PublicKeyCredentialUserEntity;
@@ -46,7 +46,7 @@ class UserCredentialSourceRepository implements PublicKeyCredentialSourceReposit
      */
     public function findOneByCredentialId(string $publicKeyCredentialId): ?PublicKeyCredentialSource
     {
-        $encodedId = Base64Url::encode($publicKeyCredentialId);
+        $encodedId = Base64Utility::basicEncode($publicKeyCredentialId);
         $credentials = $this->getUserData($this->user);
         $credential = $credentials[$encodedId] ?? null;
 
@@ -82,7 +82,7 @@ class UserCredentialSourceRepository implements PublicKeyCredentialSourceReposit
     public function saveCredentialSource(PublicKeyCredentialSource $publicKeyCredentialSource): void
     {
         $credentials = $this->getUserData($this->user);
-        $id = Base64Url::encode($publicKeyCredentialSource->getPublicKeyCredentialId());
+        $id = Base64Utility::basicEncode($publicKeyCredentialSource->getPublicKeyCredentialId());
         $credentials[$id] = json_decode(json_encode($publicKeyCredentialSource), true);
         $this->patchUserData($this->user, $credentials);
         $res = $this->usersTable->saveOrFail($this->user);
