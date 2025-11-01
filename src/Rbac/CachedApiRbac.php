958fc5dcdc686137d772b3432abe3dfdd85fde21
Yevgeny Tomenko <skie@mail.ru>
2025-11-01 11:36:47 +0300

    Improve api RBAC to support className definition. Intial cached RBAC class.

---
+++ src/Rbac/CachedApiRbac.php
@@ -0,0 +1,136 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright 2010 - 2025, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2025, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace CakeDC\Api\Rbac;
+
+use Cake\Cache\Cache;
+use Cake\Utility\Hash;
+use CakeDC\Api\Rbac\ApiRbac;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Class CachedApiRbac
+ *
+ * Cached version of ApiRbac for improved performance.
+ * Builds a permissions map based on service/action and caches it.
+ */
+class CachedApiRbac extends ApiRbac
+{
+    /**
+     * A map of rules
+     *
+     * @var array[] rules array
+     */
+    protected $permissionsMap = [];
+
+    /**
+     * CachedApiRbac constructor.
+     *
+     * @param array $config Class configuration
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+        $this->permissionsMap = Cache::remember('api_permissions_map', function () {
+            return $this->buildPermissionsMap();
+        }, '_cakedc_api_auth_');
+    }
+
+    /**
+     * Build permissions map for caching
+     *
+     * @return array
+     */
+    public function buildPermissionsMap()
+    {
+        $asArray = function ($permission, $key, $default = null) {
+            if ($default !== null && !array_key_exists($key, $permission)) {
+                return [$default, '_'];
+            }
+            if (!array_key_exists($key, $permission) || $permission[$key] == false || $permission[$key] == null) {
+                return ['_'];
+            }
+            $item = $permission[$key];
+            if (is_string($item)) {
+                return [$item];
+            }
+            return (array)$item;
+        };
+
+        $map = [];
+        foreach ($this->permissions as $permission) {
+            if (isset($permission['role'])) {
+                $role = $permission['role'];
+            } else {
+                $role = '*';
+            }
+            $roles = (array)$role;
+            foreach ($roles as $role) {
+                $services = $asArray($permission, 'service', '*');
+                foreach ($services as $service) {
+                    $key = $service;
+                    $map[$role][$key][] = $permission;
+                }
+            }
+        }
+
+        return $map;
+    }
+
+    /**
+     * Match against permissions using cached map
+     *
+     * @param array|\ArrayAccess $user current user array
+     * @param \Psr\Http\Message\ServerRequestInterface $request request
+     * @return bool true if there is a match in permissions
+     */
+    public function checkPermissions($user, ServerRequestInterface $request)
+    {
+        $roleField = $this->getConfig('role_field');
+        $defaultRole = $this->getConfig('default_role');
+        $role = Hash::get($user, $roleField, $defaultRole);
+
+        $service = $request->getAttribute('service');
+        if ($service === null) {
+            return false;
+        }
+
+        $serviceName = $service->getName();
+        $keys = [$serviceName, '*'];
+
+        foreach ([$role, '*'] as $checkRole) {
+            if (!array_key_exists($checkRole, $this->permissionsMap)) {
+                continue;
+            }
+            foreach ($keys as $key) {
+                if (!array_key_exists($key, $this->permissionsMap[$checkRole])) {
+                    continue;
+                }
+                $permissions = $this->permissionsMap[$checkRole][$key];
+                foreach ($permissions as $permission) {
+                    $matchResult = $this->_matchPermission($permission, $user, $role, $request);
+                    if ($matchResult !== null) {
+                        if ($this->getConfig('log')) {
+                            $this->log($matchResult->getReason(), LogLevel::DEBUG);
+                        }
+
+                        return $matchResult->isAllowed();
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
